c3c52cdf723c1486e55bb9943954a971408efa6d
Christina <email@christina-busacker.de>
2026-02-15 13:26:22 +0100

    remove caddy again

--- apps/lmstudio-web/src/main.ts
+++ apps/lmstudio-web/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { mkdirSync } from 'fs';
 import { join } from 'path';
