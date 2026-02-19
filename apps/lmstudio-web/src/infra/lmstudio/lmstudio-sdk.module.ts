63051125620cc85f972d81c3107f83ec6d73af0f
Christina <email@christina-busacker.de>
2026-02-19 22:35:59 +0100

    remove lmstudio sdk leftovers

--- apps/lmstudio-web/src/infra/lmstudio/lmstudio-sdk.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LmStudioClientFactory } from './lmstudio-client.factory';
-
-@Module({
-  providers: [LmStudioClientFactory],
-  exports: [LmStudioClientFactory],
-})
-export class LmStudioSdkModule {}
--- apps/lmstudio-web/src/models/models.module.ts
+++ apps/lmstudio-web/src/models/models.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { LmStudioSdkModule } from '../infra/lmstudio/lmstudio-sdk.module';
 import { ModelsService } from './models.service';
 import { ModelsController } from './models.controller';
 import { SseModule } from '../sse/sse.module';
 
 @Module({
-  imports: [LmStudioSdkModule, SseModule],
+  imports: [SseModule],
   providers: [ModelsService],
   exports: [ModelsService],
   controllers: [ModelsController],
